b5e374cfbd4d5b72e1b2c35a4c7a242233921f4f
AizadaBolotbekova <120017125+AizadaBolotbekova@users.noreply.github.com>
2023-01-18 06:30:27 +0300

    Add files via upload

--- DZ2_Aizada_21_03/main.js
+++ DZ2_Aizada_21_03/main.js
@@ -8,23 +8,23 @@ let start = 1;
 const move = () => {
   if (start === 1) {
     if (positionX_true === false && positionX <= 440) {
-      positionX += 15;
+      positionX += 25;
       block.style.left = `${positionX}px`;
-      setTimeout(move, 10);
+      setTimeout(move, 20);
     } else if (positionX >= 440 && positionY <= 440) {
-      positionY += 15;
+      positionY += 25;
       block.style.top = `${positionY}px`;
-      setTimeout(move, 10);
+      setTimeout(move, 20);
     } else if (positionX > 0 && positionY >= 440) {
       positionX_true = true;
-      positionX -= 15;
+      positionX -= 25;
       block.style.left = `${positionX}px`;
-      setTimeout(move, 10);
+      setTimeout(move, 20);
     } else if (positionX_false === false && positionY > 0) {
       if (positionY === 10) positionX_true = false;
       positionY -= 10;
       block.style.top = `${positionY}px`;
-      setTimeout(move, 10);
+      setTimeout(move, 20);
     }
   }
 };
@@ -49,46 +49,3 @@ btn.addEventListener("click",()=>{
   })
 })
 //////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////
-//
-// setTimeout(()=>{
-//     console.log("Hello world")
-// },50000)
-//
-// setInterval(()=>{
-//     console.log("Hello world")
-// },1000)
-
-/////////////////////////////////////////////////////////////////
-
-// const block = document.querySelector(".block");
-//
-// let positionX_start = 0;
-// let positionY_start = 0;
-// let positionX_end = 448;
-// let positionY_end = 448;
-//
-// const move = () => {
-//     //////////// // block.style.left = 50+"px";
-//     ////////////// block.style.left = `${250}px`
-//     if (positionX_start <= 440) {
-//         positionX_start = positionX_start + 15;
-//         block.style.left = `${positionX_start}px `;
-//         setTimeout(move, 45);
-//     } else if (positionX_start >= 440 && positionY_start <= 440) {
-//         positionY_start += 15;
-//         block.style.top = `${positionY_start}px`;
-//         setTimeout(move, 42);
-//     } else if (positionX_end > 0) {
-//         positionX_end -= 15;
-//         block.style.left = `${positionX_end}px`;
-//         setTimeout(move, 40);
-//     } else if (positionY_end > 0) {
-//         positionY_end -= 15;
-//         block.style.top = `${positionY_end}px`;
-//         setTimeout(move, 40);
-//     }
-// };
-// move();
-
